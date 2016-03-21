f39d473e9bb9b2d482bfa0cf2c0832c175048431
1486327116@qq.com <1486327116@qq.com>
2016-03-21 22:31:49 +0800

    v.1.1

--- game_1.py
+++ game_1.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python
 #coding=utf-8
+import random
+
 readme='''这是一个简单人机剪刀，石头，布的对战
 请输入你的选择：
 1 剪刀
@@ -7,16 +9,27 @@ readme='''这是一个简单人机剪刀，石头，布的对战
 3 布
 
 '''
-import random
-choice_list=['剪刀','石头','布']
-win_list=['剪刀布','石头剪刀','布石头']
-player=choice_list[int(raw_input(readme))]
-computer=random.choice(choice_list)
-print '你选择了{you}，电脑选择了{computer}'.format(you=player,computer=computer)
 
-if player is computer:
-    print 'no winner...'
-elif player + computer in win_list:
-    print '\033[32mYOU WIN!!!\033[0m'
+choice_list = ['剪刀','石头','布']
+win_list = ['剪刀布','石头剪刀','布石头']
+times = 0
+win_times = 0
+while times < 3:
+    player = choice_list[int(raw_input(readme))]
+    computer = random.choice(choice_list)
+    print '你选择了{you}，电脑选择了{computer}'.format(you=player,computer=computer)
+
+    if player is computer:
+        print 'no winner...'
+    elif player + computer in win_list:
+        print '\033[32mIN {} TIME,YOU WIN!!!\033[0m'.format(times+1)
+        times += 1
+        win_times += 1
+    else:
+        print '\033[31mIN {} TIME,YOU LOSE...\033[0m'.format(times+1)
+        times += 1
+
+if win_times >= 2:
+    print '\n\033[32m############FINALLY,YOU WIN!!!###############\033[0m'
 else:
-    print '\033[31mYOU LOSE...\033[0m'
+    print '\n\033[31m############FINALLY,YOU LOSE!!!###############\033[0m'
